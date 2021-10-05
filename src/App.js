d60349fa0969ebe4267278020745c10523489fe6
vindygao1997 <vindygao1997@gmail.com>
2021-10-05 00:25:04 -0500

    first github commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    //bem convention
+    <div className="app">
+      <h1>Hello</h1>
+      {/* Header */}
+      {/* Home */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyC9C9IEDflDS4y3elEHVsr6TK275V7v1oA",
+    authDomain: "clone-15db3.firebaseapp.com",
+    projectId: "clone-15db3",
+    storageBucket: "clone-15db3.appspot.com",
+    messagingSenderId: "329373157068",
+    appId: "1:329373157068:web:31d6b2e0321483a579a469",
+    measurementId: "G-ME6JJ9B8W9"
+  };
+  
